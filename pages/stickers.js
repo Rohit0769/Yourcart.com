692430b9d93e18c66390c5126531250506e6f097
Rohit0769 <rohitgupta0769@gmail.com>
2024-03-07 17:40:05 +0530

    this is commit

--- pages/stickers.js
+++ pages/stickers.js
@@ -52,6 +52,7 @@ export async function getServerSideProps(context) {
   if (!mongoose.connections[0].readyState) {
 
     await mongoose.connect(process.env.MONGO_URI)
+     console.log(process.env.MONGO_URI)
   }
   let products = await Product.find({ category: "Pants" })
   let Pants = {};
